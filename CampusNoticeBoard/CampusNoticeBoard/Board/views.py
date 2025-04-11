b754376840a97389977b3a3eb09f73e7706dcd64
FrezerMakina <104712102+FrezerMakina@users.noreply.github.com>
2025-04-11 21:57:02 +0200

    Created a simple model, superuser and a template

--- CampusNoticeBoard/CampusNoticeBoard/Board/views.py
+++ CampusNoticeBoard/CampusNoticeBoard/Board/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView
 
 # Create your views here.
+class PostView(ListView):
+    model = Post
+    template_name = "Board/index.html"
+    context_object_name = "posts"
+
