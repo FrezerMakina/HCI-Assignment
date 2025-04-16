e549d717289b3c61a8f44a9e2ac7ee9a49c53a03
FrezerMakina <104712102+FrezerMakina@users.noreply.github.com>
2025-04-16 19:53:54 +0200

    Added all CRUD functionalities to the API

--- CampusNoticeBoard/CampusNoticeBoard/Board/views.py
+++ CampusNoticeBoard/CampusNoticeBoard/Board/views.py
@@ -14,3 +14,30 @@ class StudentViewset(viewsets.ViewSet):
         queryset = Student.objects.all()
         serializer = self.serializer_class(queryset, many=True)
         return Response(serializer.data)
+    
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=400)
+        
+    def retrieve(self, request, pk=None):
+        queryset = self.queryset.get(pk=pk)
+        serializer = self.serializer_class(queryset)
+        return Response(serializer.data)
+    
+    def update(self, request, pk=None):
+        queryset = self.queryset.get(pk=pk)
+        serializer = self.serializer_class(queryset, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=400)
+        
+    def destroy(self, request, pk=None):
+        queryset = self.queryset.get(pk=pk)
+        queryset.delete()
+        return Response(status=204)
