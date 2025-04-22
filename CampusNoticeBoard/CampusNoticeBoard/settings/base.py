7f10f06cce46eae516717206f64728ec710036a6
yankhofunsani <funsaniyankho@gmail.com>
2025-04-22 12:12:23 +0200

    back to default

--- CampusNoticeBoard/CampusNoticeBoard/settings/base.py
+++ CampusNoticeBoard/CampusNoticeBoard/settings/base.py
@@ -81,11 +81,15 @@ WSGI_APPLICATION = 'CampusNoticeBoard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        
-       }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'BdPRXqXHnKgGsfLIooMNgXymTiNVUKoD',
+        'HOST': 'gondola.proxy.rlwy.net',
+        'PORT': '10656',
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
